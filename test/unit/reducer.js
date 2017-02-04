ec5efc490c54ec99e456d69ef442205598681fb8
James Smith <jellyes2@gmail.com>
2017-02-03 16:14:19 -0800

    Reorganize reducers tests

--- test/unit/reducer.js
+++ test/unit/reducer.js
@@ -7,6 +7,7 @@ describe('reducer', function() {
     expect(result.reducer).to.be.a('function');
   });
 
+  // Todo...move this.
   it('should manage CREATE', () => {
     const result = reduxInconsistentApi('hello');
     const reduced = result.reducer(result.initialState, {
@@ -17,110 +18,6 @@ describe('reducer', function() {
       resources: [],
       resourcesMeta: {},
       resourcesListMeta: {}
-    });
-  });
-
-  describe('readMany', () => {
-    it('should handle `RETRIEVE_HELLOS`', () => {
-      const result = reduxInconsistentApi('hello');
-      const reduced = result.reducer(result.initialState, {
-        type: 'RETRIEVE_HELLOS'
-      });
-
-      expect(reduced).to.deep.equal({
-        resources: [],
-        resourcesMeta: {},
-        resourcesListMeta: {
-          retrievingStatus: resourceStatuses.PENDING
-        }
-      });
-    });
-
-    it('should handle `RETRIEVE_HELLOS_FAILURE`', () => {
-      const result = reduxInconsistentApi('hello');
-      const reduced = result.reducer(result.initialState, {
-        type: 'RETRIEVE_HELLOS_FAILURE'
-      });
-
-      expect(reduced).to.deep.equal({
-        resources: [],
-        resourcesMeta: {},
-        resourcesListMeta: {
-          retrievingStatus: resourceStatuses.FAILED
-        }
-      });
-    });
-
-    it('should handle `RETRIEVE_HELLOS_SUCCESS`', () => {
-      const result = reduxInconsistentApi('hello');
-      const reduced = result.reducer(result.initialState, {
-        type: 'RETRIEVE_HELLOS_SUCCESS',
-        resources: [
-          {id: 2, hungry: true, pasta: 'yespls'},
-          {id: 100, hungry: false},
-        ]
-      });
-
-      expect(reduced).to.deep.equal({
-        resources: [
-          {id: 2, hungry: true, pasta: 'yespls'},
-          {id: 100, hungry: false},
-        ],
-        resourcesMeta: {
-          2: {
-            updatingStatus: null,
-            isDeleting: false
-          },
-          100: {
-            updatingStatus: null,
-            isDeleting: false
-          }
-        },
-        resourcesListMeta: {
-          retrievingStatus: resourceStatuses.SUCCESS
-        }
-      });
-    });
-
-    it('should handle `RETRIEVE_HELLOS_ABORTED`', () => {
-      const result = reduxInconsistentApi('hello');
-      const reduced = result.reducer(result.initialState, {
-        type: 'RETRIEVE_HELLOS_ABORTED'
-      });
-
-      expect(reduced).to.deep.equal({
-        resources: [],
-        resourcesMeta: {},
-        resourcesListMeta: {
-          retrievingStatus: resourceStatuses.ABORTED
-        }
-      });
-    });
-
-    it('should handle `RETRIEVE_HELLOS_RESET_RESOLUTION`', () => {
-      const result = reduxInconsistentApi('hello');
-
-      // We set some value on `retrievingStatus` to check that this nulls it
-      const resourcesListMetaState = {
-        resourcesListMeta: {
-          retrievingStatus: 'sandwiches'
-        }
-      };
-
-      const reduced = result.reducer({
-        ...result.initialState,
-        ...resourcesListMetaState
-      }, {
-        type: 'RETRIEVE_HELLOS_RESET_RESOLUTION'
-      });
-
-      expect(reduced).to.deep.equal({
-        resources: [],
-        resourcesMeta: {},
-        resourcesListMeta: {
-          retrievingStatus: null
-        }
-      });
     });
   });
 });
