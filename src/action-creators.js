1a17860a457325e69ea275d410d138deea3956e6
James Smith <jellyes2@gmail.com>
2017-02-09 17:16:27 -0800

    Begin scaffolding action creators

--- src/action-creators.js
+++ src/action-creators.js
@@ -1,29 +1,29 @@
 import xhr from 'xhr';
+import * as actionCreators from './default-action-creators';
 
-export default function(actionTypes) {
-  return {
-    create() {
-      // return (dispatch, getState) => {
-      //   dispatch({type: actionTypes.CREATE});
-      // };
-    },
+export default function(allowedCrudOperations) {
+  const {create, readOne, readMany, update, del} = allowedCrudOperations;
+  const actionCreators = {};
 
-    read() {
-      return (dispatch, getState) => {
-
-      };
-    },
-
-    update() {
-      return (dispatch, getState) => {
-
-      };
-    },
-
-    delete() {
-      return (dispatch, getState) => {
-
-      };
-    },
+  if (create) {
+    actionCreators.create = actionCreators.create;
   }
+
+  if (readOne) {
+    actionCreators.readOne = actionCreators.readOne;
+  }
+
+  if (readMany) {
+    actionCreators.readMany = actionCreators.readMany;
+  }
+
+  if (update) {
+    actionCreators.update = actionCreators.update;
+  }
+
+  if (del) {
+    actionCreators.del = actionCreators.del;
+  }
+
+  return actionCreators;
 }
---
+++ src/default-action-creators/index.js
@@ -0,0 +1,5 @@
+export {create} from './create';
+export {readOne} from './read-one';
+export {readMany} from './read-many';
+export {update} from './update';
+export {del} from './delete';
