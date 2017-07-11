eb00165f22d14f54c8430b00d15e618c87583b28
Jmeas <jellyes2@gmail.com>
2017-07-10 21:51:52 -0700

    Ensure action type is always logged for bad actions

--- packages/resourceful-redux/src/utils/reducer-generator.js
+++ packages/resourceful-redux/src/utils/reducer-generator.js
@@ -37,10 +37,11 @@ export default function(crudAction, requestStatus) {
     if (!label && !idList.length) {
       if (process.env.NODE_ENV !== 'production') {
         console.warn(
-          `A resourceful-redux action was dispatched without a "label" or ` +
-          `a "resources" array. Without one of these values, Resourceful Redux ` +
-          `cannot track your CRUD operation. You should check your Action Creators. ` +
-          `Read more about CRUD Actions at: https://resourceful-redux.js.org/docs/guides/crud-actions.html`
+          `A Resourceful Redux action of type ${action.type} was dispatched ` +
+          `without a "label" or a "resources" array. Without one of these ` +
+          `values, Resourceful Redux cannot track your CRUD operation. You ` +
+          `should check your Action Creators. Read more about CRUD Actions ` +
+          `at: https://resourceful-redux.js.org/docs/guides/crud-actions.html`
         );
       }
 
