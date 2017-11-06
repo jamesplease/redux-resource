c3145a327d55231e24edf8b0ee2b88767e1f35b7
Stephen Rivas Jr <sprjr@me.com>
2017-11-06 15:22:14 -0800

    Introduce redux-resource-action-creators

---
+++ packages/redux-resource-action-creators/src/index.js
@@ -0,0 +1,63 @@
+import {actionTypes} from 'redux-resource';
+import warning from './utils/warning';
+
+const _createAction = function(actionProperties = {}, actionDefaults = {}, type) {
+  if (process.env.NODE_ENV !== 'production') {
+    if (actionProperties.type || actionDefaults.type) {
+      warning(
+        'It looks like you provided a `type` property for an action created using redux-resource-' +
+        'action-creators. This library sets the `type` for you; your value has been ignored. ' +
+        'For more, refer to the documentation: ' +
+        'https://redux-resource.js.org/docs/extras/redux-resource-action-creators.html'
+      );
+    }
+  }
+
+  return {
+    ...actionDefaults,
+    ...actionProperties,
+    type
+  };
+};
+
+const createActionCreators = (crudType, actionDefaults = {}) => {
+  const {resourceName} = actionDefaults;
+  const uppercaseCrud = typeof crudType === 'string' ? crudType.toUpperCase() : '';
+  const isValidCrudType =
+    uppercaseCrud === 'UPDATE' || uppercaseCrud === 'DELETE' ||
+    uppercaseCrud === 'READ' || uppercaseCrud === 'CREATE';
+
+  if (process.env.NODE_ENV !== 'production') {
+    if (!isValidCrudType) {
+      warning(
+        `You supplied an invalid crudType of ${crudType} to createActionCreators.` +
+        `Please verify you've supplied one of: ['read', 'update', 'delete', 'create'].` +
+        'For more, refer to the documentation: ' +
+        'https://redux-resource.js.org/docs/extras/redux-resource-action-creators.html'
+      );
+    }
+
+    if (typeof resourceName !== 'string') {
+      warning(
+        `The value of "resourceName" that you passed to createActionCreators was ` +
+        `not a string. The resourceName must be a string. You should check ` +
+        `your redux-resource-action-creators configuration.` +
+        'For more, refer to the documentation: ' +
+        'https://redux-resource.js.org/docs/extras/redux-resource-action-creators.html'
+      );
+    }
+  }
+
+  return {
+    pending: (actionProperties) =>
+      _createAction(actionProperties, actionDefaults, actionTypes[`${uppercaseCrud}_RESOURCES_PENDING`]),
+    null: (actionProperties) =>
+      _createAction(actionProperties, actionDefaults, actionTypes[`${uppercaseCrud}_RESOURCES_NULL`]),
+    succeeded: (actionProperties) =>
+      _createAction(actionProperties, actionDefaults, actionTypes[`${uppercaseCrud}_RESOURCES_SUCCEEDED`]),
+    failed: (actionProperties) =>
+      _createAction(actionProperties, actionDefaults, actionTypes[`${uppercaseCrud}_RESOURCES_FAILED`]),
+  };
+};
+
+export default createActionCreators;
