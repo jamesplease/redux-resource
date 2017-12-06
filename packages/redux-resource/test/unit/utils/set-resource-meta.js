486da2635dc0c3707d96bb58c89c05514adc37f4
James Smith <jamesplease2@gmail.com>
2017-12-06 13:27:29 -0800

    Support resources object in setResourceMeta

--- packages/redux-resource/test/unit/utils/set-resource-meta.js
+++ packages/redux-resource/test/unit/utils/set-resource-meta.js
@@ -38,6 +38,32 @@ describe('setResourceMeta', function() {
       expect(result[2]).to.not.equal(this.meta[2]);
     });
 
+    it('should only keep metadata that is passed in when `resources` is an object', () => {
+      const result = setResourceMeta({
+        meta: this.meta,
+        newMeta: {isSelected: true},
+        resources: {
+          1: {id: 1},
+          2: {id: 2},
+          5: {id: 5}
+        },
+        mergeMeta: false
+      });
+
+      expect(result).to.deep.equal({
+        1: {isSelected: true},
+        2: {isSelected: true},
+        3: {what: false},
+        5: {isSelected: true}
+      });
+
+      // The original `meta` is shallow cloned
+      expect(result).to.not.equal(this.meta);
+      // The existing item is not modified
+      expect(result[1]).to.not.equal(this.meta[1]);
+      expect(result[2]).to.not.equal(this.meta[2]);
+    });
+
     it('should ignore badly formed resources, object form (gh-118)', () => {
       const result = setResourceMeta({
         meta: this.meta,
@@ -85,6 +111,40 @@ describe('setResourceMeta', function() {
         meta: this.meta,
         newMeta: {isSelected: true},
         resources: [1, 2, 5],
+        mergeMeta: true
+      });
+
+      expect(result).to.deep.equal({
+        1: {
+          isSelected: true,
+          otherData: {
+            hungry: true,
+            food: 'pizza'
+          }
+        },
+        2: {thirsty: true, isSelected: true},
+        3: {what: false},
+        5: {isSelected: true}
+      });
+
+      // The original `meta` is shallow cloned
+      expect(result).to.not.equal(this.meta);
+      // The existing item is not modified
+      expect(result[1]).to.not.equal(this.meta[1]);
+      expect(result[2]).to.not.equal(this.meta[2]);
+      // Unrelated items pass through unchanged
+      expect(result[3]).to.equal(this.meta[3]);
+    });
+
+    it('should keep other list items and merge in the new results when `resources` is an object', () => {
+      const result = setResourceMeta({
+        meta: this.meta,
+        newMeta: {isSelected: true},
+        resources: {
+          1: {id: 1},
+          2: {id: 2},
+          5: {id: 5}
+        },
         mergeMeta: true
       });
 
