6fef45866e72911ff4ca14575906ab9dcbeb61d1
James <jamesplease@users.noreply.github.com>
2019-10-15 08:11:38 -0700

    Add byId option to getResources

--- packages/redux-resource/src/utils/get-resources.js
+++ packages/redux-resource/src/utils/get-resources.js
@@ -1,14 +1,38 @@
 import warning from './warning';
 
+function resourceArrayToObject(arr) {
+  return arr.reduce((memo, r) => {
+    memo[r.id] = r;
+    return memo;
+  }, {});
+}
+
 // Returns a list of resources by IDs or list name
-export default function(resourceSlice, filter) {
+export default function(resourceSlice, filter, options) {
   if (process.env.NODE_ENV !== 'production') {
-    if (arguments.length === 3) {
+    // We use duck typing to try and differentiate between a user passing the entire state tree
+    // in as `resourceSlice` (which was the original getResources API).
+    // eslint-disable-next-line no-inner-declarations
+    function validateSlice(slice = {}) {
+      return (
+        slice.hasOwnProperty('resources') &&
+        slice.hasOwnProperty('meta') &&
+        slice.hasOwnProperty('requests') &&
+        slice.hasOwnProperty('lists')
+      );
+    }
+
+    if (
+      arguments.length === 3 &&
+      !validateSlice(resourceSlice) &&
+      // We assume that undefined means that a slice is undefined, rather than the entire state tree being undefined.
+      typeof resourceSlice !== 'undefined'
+    ) {
       warning(
         `You called getResources with an argument signature that was removed in ` +
-          `v3.0.0 of Redux Resource. The old signature accepted three arguments. The new ` +
-          `signature only accepts two. Please update your code to use the new signature. ` +
-          `For more information, reference the documentation at ` +
+          `v3.0.0 of Redux Resource. The old signature accepted three arguments, with the first being the all of the store's ` +
+          `state. The new signature of this function requires passing a resource slice as the first argument. Please ` +
+          `update your code to use the new signature. For more information, reference the documentation at ` +
           `https://redux-resource.js.org/docs/api-reference/get-resources.html\n\n` +
           `Also, the migration guide to Redux Resource v3 can be found at: ` +
           `https://github.com/jamesplease/redux-resource/blob/master/packages/redux-resource/docs/migration-guides/2-to-3.md`,
@@ -17,8 +41,15 @@ export default function(resourceSlice, filter) {
     }
   }
 
+  let byId;
+  if (options && options.byId) {
+    byId = options.byId;
+  }
+
+  const noResultsReturn = byId ? {} : [];
+
   if (!resourceSlice) {
-    return [];
+    return noResultsReturn;
   }
 
   const resources = resourceSlice.resources;
@@ -26,14 +57,16 @@ export default function(resourceSlice, filter) {
 
   if (typeof filter === 'function' || !filter) {
     const appliedFilter = filter ? filter : () => true;
-    return Object.values(resources).filter(resource =>
+    const resourcesList = Object.values(resources).filter(resource =>
       appliedFilter(resource, resourceSlice.meta[resource.id], resourceSlice)
     );
+
+    return byId ? resourceArrayToObject(resourcesList) : resourcesList;
   } else if (typeof filter === 'string') {
     // This conditional handles the situation where `filter` is an list name
     const list = resourceSlice.lists[filter];
     if (!list) {
-      return [];
+      return noResultsReturn;
     }
 
     idsList = list;
@@ -42,8 +75,10 @@ export default function(resourceSlice, filter) {
   }
 
   if (!(idsList && idsList.length)) {
-    return [];
+    return noResultsReturn;
   }
 
-  return idsList.map(id => resources[id]).filter(Boolean);
+  const resourcesList = idsList.map(id => resources[id]).filter(Boolean);
+
+  return byId ? resourceArrayToObject(resourcesList) : resourcesList;
 }
--- packages/redux-resource/test/unit/utils/get-resources.js
+++ packages/redux-resource/test/unit/utils/get-resources.js
@@ -10,32 +10,38 @@ describe('getResources', function() {
           1: { id: 1, name: 'sandwiches' },
           10: { id: 10, name: 'pizza' },
           102: { id: 102, name: 'fried' },
-          116: { id: 116, name: 'pickles' }
+          116: { id: 116, name: 'pickles' },
         },
         meta: {
           1: {},
           10: {
-            selected: true
+            selected: true,
           },
           102: {
-            selected: false
+            selected: false,
           },
           116: {
-            selected: true
-          }
+            selected: true,
+          },
         },
         lists: {
           dashboardSearch: [10, 22, 102],
           detailsPage: [],
-          malformedList: {}
-        }
+          malformedList: {},
+        },
+        requests: {},
       },
       movies: {
         resources: {},
         meta: {},
-        requests: {}
-      }
+        requests: {},
+        lists: {},
+      },
     };
+  });
+
+  afterEach(() => {
+    console.error.reset();
   });
 
   describe('when a nonexistent slice is passed', () => {
@@ -43,11 +49,25 @@ describe('getResources', function() {
       expect(getResources(this.state.yum, [1])).to.deep.equal([]);
       expect(console.error.callCount).to.equal(0);
     });
+
+    it('byId: should return an empty object', () => {
+      expect(getResources(this.state.yum, [1], { byId: true })).to.deep.equal(
+        {}
+      );
+      expect(console.error.callCount).to.equal(0);
+    });
   });
 
   describe('when the resources object is empty', () => {
     it('should return an empty array', () => {
       expect(getResources(this.state.movies, [1])).to.deep.equal([]);
+      expect(console.error.callCount).to.equal(0);
+    });
+
+    it('byId: should return an empty object', () => {
+      expect(
+        getResources(this.state.movies, [1], { byId: true })
+      ).to.deep.equal({});
       expect(console.error.callCount).to.equal(0);
     });
   });
@@ -71,9 +91,42 @@ describe('getResources', function() {
     it('should return the resources that exist when the list exists', () => {
       expect(getResources(this.state.books, 'dashboardSearch')).to.deep.equal([
         { id: 10, name: 'pizza' },
-        { id: 102, name: 'fried' }
+        { id: 102, name: 'fried' },
       ]);
       expect(console.error.callCount).to.equal(0);
+    });
+
+    describe('byId', () => {
+      it('should return an empty array when the list does not exist', () => {
+        expect(
+          getResources(this.state.books, 'sandwiches', { byId: true })
+        ).to.deep.equal({});
+        expect(console.error.callCount).to.equal(0);
+      });
+
+      it('should return an empty array when the list exists, and has no IDs', () => {
+        expect(
+          getResources(this.state.books, 'detailsPage', { byId: true })
+        ).to.deep.equal({});
+        expect(console.error.callCount).to.equal(0);
+      });
+
+      it('should return an empty array when the list exists, but does not have an IDs array (this should never happen!)', () => {
+        expect(
+          getResources(this.state.books, 'malformedList', { byId: true })
+        ).to.deep.equal({});
+        expect(console.error.callCount).to.equal(0);
+      });
+
+      it('should return the resources that exist when the list exists', () => {
+        expect(
+          getResources(this.state.books, 'dashboardSearch', { byId: true })
+        ).to.deep.equal({
+          10: { id: 10, name: 'pizza' },
+          102: { id: 102, name: 'fried' },
+        });
+        expect(console.error.callCount).to.equal(0);
+      });
     });
   });
 
@@ -86,7 +139,7 @@ describe('getResources', function() {
     it('should return the resources that when the IDs array is non-empty', () => {
       expect(getResources(this.state.books, [1, 116, 130])).to.deep.equal([
         { id: 1, name: 'sandwiches' },
-        { id: 116, name: 'pickles' }
+        { id: 116, name: 'pickles' },
       ]);
       expect(console.error.callCount).to.equal(0);
     });
@@ -119,8 +172,43 @@ describe('getResources', function() {
 
       expect(getResources(this.state.books, filter)).to.deep.equal([
         { id: 10, name: 'pizza' },
-        { id: 116, name: 'pickles' }
+        { id: 116, name: 'pickles' },
       ]);
+      expect(console.error.callCount).to.equal(0);
+    });
+
+    it('byId: should return an empty object when nothing matches a filtering function', () => {
+      const filter = (resource, meta, resourceSlice) => {
+        const resourceId = resource.id;
+        expect(resource).to.deep.equal(this.state.books.resources[resourceId]);
+        expect(resourceSlice).to.deep.equal(this.state.books);
+        expect(meta).to.deep.equal(this.state.books.meta[resourceId]);
+
+        return false;
+      };
+
+      expect(
+        getResources(this.state.books, filter, { byId: true })
+      ).to.deep.equal({});
+      expect(console.error.callCount).to.equal(0);
+    });
+
+    it('byId: should return the resources that match a filtering function', () => {
+      const filter = (resource, meta, resourceSlice) => {
+        const resourceId = resource.id;
+        expect(resource).to.deep.equal(this.state.books.resources[resourceId]);
+        expect(resourceSlice).to.deep.equal(this.state.books);
+        expect(meta).to.deep.equal(this.state.books.meta[resourceId]);
+
+        return meta.selected;
+      };
+
+      expect(
+        getResources(this.state.books, filter, { byId: true })
+      ).to.deep.equal({
+        10: { id: 10, name: 'pizza' },
+        116: { id: 116, name: 'pickles' },
+      });
       expect(console.error.callCount).to.equal(0);
     });
   });
@@ -139,10 +227,9 @@ describe('getResources', function() {
     it('should warn', () => {
       try {
         getResources(this.state, 'books', [10]);
-      } catch(e) {
+      } catch (e) {
         // Intentionally blank
       }
-
 
       expect(console.error.callCount).to.equal(1);
     });
